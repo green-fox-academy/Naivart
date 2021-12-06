83e15f98a11f75564181416d136606339fbafe62
AdamSattler <adamsat@seznam.cz>
2021-12-06 10:09:57 +0100

    first commit

---
+++ Naivart/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Naivart.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Naivart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public Service Service { get; set; }
+        public HomeController(Service service)
+        {
+            Service = service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Naivart/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Naivart.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Naivart
+{
+    public class Startup
+    {
+        private IConfiguration AppConfig { get; }
+
+        public Startup(IConfiguration config)
+        {
+            AppConfig = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            ConfigureDb(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private void ConfigureDb(IServiceCollection services)
+        {
+            var connectionString = AppConfig.GetConnectionString("DefaultConnection");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0));
+
+            services.AddDbContext<ApplicationDbContext>(
+                options => options
+                .UseMySql(connectionString, serverVersion)
+                // The following three options help with debugging
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors());
+        }
+    }
+}
