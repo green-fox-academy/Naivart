cbb01d690147d4c83e75c019891a539c5044cef6
Jan Malý <janmaly90@gmail.com>
2021-12-09 09:37:48 +0100

    Add LoginService into Startup and delete Service.

--- Naivart/Startup.cs
+++ Naivart/Startup.cs
@@ -1,17 +1,17 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using Naivart.Database;
-using Naivart.Models;
 using Naivart.Services;
 using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Naivart
 {
@@ -29,34 +29,8 @@ namespace Naivart
             services.AddAutoMapper(typeof(Startup));
             services.AddTransient<KingdomService>();
             services.AddControllersWithViews();
-
-            services.AddTransient<LoginService>();
             services.AddTransient<PlayerService>();
-
-            var appSettingSection = AppConfig.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingSection);
-
-            var appSettings = appSettingSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.key);
-
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-
-                };
-            });
-
+            services.AddTransient<LoginService>();
             ConfigureDb(services);
         }
 
@@ -71,9 +45,6 @@ namespace Naivart
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
