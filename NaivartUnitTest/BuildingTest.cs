4d1df3d6379be6ca3df252ec2ce691dd8d0753f0
PavelBla <pavel.blazekk@gmail.com>
2021-12-13 08:38:15 +0100

    commit for merging master

--- NaivartUnitTest/BuildingTest.cs
+++ NaivartUnitTest/BuildingTest.cs
@@ -17,5 +17,31 @@ namespace NaivartUnitTest
         {
             httpClient = factory.CreateClient();
         }
+        public void BuildingGetEndpoint_ShouldReturnOk()
+        {
+            var request = new HttpRequestMessage();
+
+            var inputObj = JsonConvert.SerializeObject(new KingdomResponseForBuilding() { KingdomId = 1 }); 
+            StringContent requestContent = new(inputObj, Encoding.UTF8, "application/json");
+            request.RequestUri = new Uri("https://localhost:44385/kingdoms/1/buildings");
+            request.Method = HttpMethod.Get;
+            request.Content = requestContent;
+            var response = httpClient.SendAsync(request).Result;
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        public void BuildingGetEndpoint_ShouldReturn401()
+        {
+            var request = new HttpRequestMessage();
+
+            var inputObj = JsonConvert.SerializeObject(new KingdomResponseForBuilding() { KingdomId = 0 });
+            StringContent requestContent = new(inputObj, Encoding.UTF8, "application/json");
+            request.RequestUri = new Uri("https://localhost:44385/kingdoms/1/buildings");
+            request.Method = HttpMethod.Get;
+            request.Content = requestContent;
+            var response = httpClient.SendAsync(request).Result;
+
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
     }
 }
