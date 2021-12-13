991745baf8a05368d91718da2c17d326b4e37bbe
AdamSattler <adamsat@seznam.cz>
2021-12-13 17:08:38 +0100

    fixed code

--- Naivart/Services/KingdomService.cs
+++ Naivart/Services/KingdomService.cs
@@ -6,7 +6,6 @@ using Naivart.Models.Entities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Naivart.Models.APIModels;
 using Naivart.Models;
 using Microsoft.Extensions.Options;
 using System;
