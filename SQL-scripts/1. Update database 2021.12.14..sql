145ca2052c927d41fc4c8bd5e7110bca152a4673
Miroslav Jiranek <jiranekmiroslav@gmail.com>
2021-12-14 11:16:34 +0100

    database update

---
+++ SQL-scripts/1. Update database 2021.12.14..sql
@@ -0,0 +1,13 @@
+	  USE `naivart`;
+		CREATE TABLE `Troops` (
+          `Id` bigint NOT NULL AUTO_INCREMENT,
+          `Level` int NOT NULL,
+          `Hp` int NOT NULL,
+          `Attack` int NOT NULL,
+          `Defense` int NOT NULL,
+          `Started_at` bigint NOT NULL,
+          `Finished_at` bigint NOT NULL,
+          `KingdomId` bigint NOT NULL,
+          CONSTRAINT `PK_Troops` PRIMARY KEY (`Id`),
+          CONSTRAINT `FK_Troops_Kingdoms_KingdomId` FOREIGN KEY (`KingdomId`) REFERENCES `Kingdoms` (`Id`) ON DELETE CASCADE
+      ) CHARACTER SET utf8mb4;
