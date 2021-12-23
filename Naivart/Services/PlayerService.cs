fdd51e5393ebea38cac293a73541fd003b94010d
AdamSattler <adamsat@seznam.cz>
2021-12-23 11:19:39 +0100

    create basic buildings when kingdom created

--- Naivart/Services/PlayerService.cs
+++ Naivart/Services/PlayerService.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Naivart.Database;
+using Naivart.Models.BuildingTypes;
 using Naivart.Models.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,39 +12,42 @@ namespace Naivart.Services
 {
     public class PlayerService
     {
+        private readonly IMapper mapper;
         private ApplicationDbContext DbContext { get; }
-        public PlayerService(ApplicationDbContext dbContext)
+        public PlayerService(ApplicationDbContext dbContext, IMapper mapper)
         {
             DbContext = dbContext;
+            this.mapper = mapper;
         }
 
         public Player RegisterPlayer(string username, string password, string kingdomName)
         {
-                if (password.Length >= 8 
-                    && !String.IsNullOrWhiteSpace(username) 
-                    && !isInDbWithThisUsername(username))
-                {
-                    Player player = new Player() { Username = username, Password = password };
-                    Kingdom kingdom = new Kingdom();
+            if (password.Length >= 8 
+                && !String.IsNullOrWhiteSpace(username) 
+                && !isInDbWithThisUsername(username))
+            {
+                Player player = new Player() { Username = username, Password = password };
+                Kingdom kingdom = new Kingdom();
 
-                //check if given kingdom name(and username) is not empty or already exists in database 
-                kingdom.Name = !String.IsNullOrWhiteSpace(kingdomName) && FindKingdomByName(kingdomName) == null 
-                               ? kingdomName : $"{player.Username}'s kingdom";
+            //check if given kingdom name(and username) is not empty or already exists in database 
+            kingdom.Name = !String.IsNullOrWhiteSpace(kingdomName) && FindKingdomByName(kingdomName) == null 
+                            ? kingdomName : $"{player.Username}'s kingdom";
 
-                var newKingdom = DbContext.Kingdoms.Add(kingdom).Entity;
-                    DbContext.SaveChanges();
+            var newKingdom = DbContext.Kingdoms.Add(kingdom).Entity;
+                DbContext.SaveChanges();
 
-                    var DbKingdom = FindKingdomByName(kingdom.Name);
-                    player.KingdomId = DbKingdom.Id;
-                    var newPlayer = DbContext.Players.Add(player).Entity;
-                    DbContext.SaveChanges();
+                var DbKingdom = FindKingdomByName(kingdom.Name);
+                player.KingdomId = DbKingdom.Id;
+                var newPlayer = DbContext.Players.Add(player).Entity;
+                DbContext.SaveChanges();    
+                CreateBasicBuidlings(DbKingdom.Id);             //creates basic buildings and save to Db 
 
-                    return DbContext.Players.Include(x=>x.Kingdom).FirstOrDefault(x => x.Username == username && x.Password == password);
-                }
-                else
-                {
-                    return null;
-                }
+                return DbContext.Players.Include(x=>x.Kingdom).FirstOrDefault(x => x.Username == username && x.Password == password);
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public Kingdom FindKingdomByName(string kingdomName)
@@ -72,7 +77,24 @@ namespace Naivart.Services
 
                 return null;
             }
+        }
+
+        public void CreateBasicBuidlings(long kingdomId)
+        {
+            DbContext.Buildings.Add(new Building() { Type = "townhall", Hp=50, Level = 1, StartedAt = 12345789, FinishedAt = 12399999, KingdomId = kingdomId });
+            DbContext.SaveChanges();
             
+            var farm = new Farm();
+            var kingdomFarm = mapper.Map<Building>(farm);
+            kingdomFarm.KingdomId = kingdomId;
+            DbContext.Buildings.Add(kingdomFarm);
+            DbContext.SaveChanges();
+            
+            var mine = new Mine();
+            var kingdomMine = mapper.Map<Building>(mine);
+            kingdomMine.KingdomId = kingdomId;
+            DbContext.Buildings.Add(kingdomMine);
+            DbContext.SaveChanges();
         }
     }
 }
