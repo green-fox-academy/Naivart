611defe8ae395f54bdd2638728dcb0d06ba76f2e
AdamSattler <adamsat@seznam.cz>
2022-01-07 10:49:02 +0100

    dead troops for defender if lose solved

--- Naivart/Services/ForKingdom/TimeService.cs
+++ Naivart/Services/ForKingdom/TimeService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Naivart.Database;
+using Naivart.Models.Entities;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Naivart.Services
@@ -39,69 +41,134 @@ namespace Naivart.Services
 
         public void UpdateBattle(long kingdomId)
         {
-            var battles = DbContext.Battles.Where(x => x.AttackerId == kingdomId || x.DefenderId == kingdomId)
-                                    .Include(x => x.AttackingTroops)?.Include(x => x.DeadTroops).ToList();
-            foreach (var battle in battles)
+            //TODO there should be security so it wont give exception
+            //check if kingdom is in or have any battles
+            if (DbContext.Battles.Any(x => x.AttackerId == kingdomId || x.DefenderId == kingdomId)) 
             {
-                int totalDamage = 0;
-                int totalDefense = 0;
-                if (battle.Result is null && battle.FinishedAt <= GetUnixTimeNow()) //If fight didn't start yet and it's already time for it
+                var battles = DbContext.Battles.Where(x => x.AttackerId == kingdomId || x.DefenderId == kingdomId)
+                                        .Include(x => x.AttackingTroops)?.Include(x => x.DeadTroops).ToList();
+                foreach (var battle in battles)
                 {
-                    foreach (var troops in battle.AttackingTroops)
+                    int totalDamage = 0;
+                    int totalDefense = 0;
+                    if (battle.Result is null && battle.FinishedAt <= GetUnixTimeNow()) //If fight didn't start yet and it's already time for it
                     {
-                        totalDamage += DbContext.TroopTypes.FirstOrDefault(x => x.Type == troops.Type && x.Level == troops.Level).Attack 
-                                        * 6 * troops.Quantity;      //total damage is calculated based on attack * quantity * 6
-                    }
-                    var defender = DbContext.Kingdoms.Where(x => x.Id == battle.DefenderId).Include(x => x.Troops)
-                                    .ThenInclude(x => x.TroopType).Include(x => x.Resources).FirstOrDefault();
+                        foreach (var troops in battle.AttackingTroops)
+                        {
+                            totalDamage += DbContext.TroopTypes.FirstOrDefault(x => x.Type == troops.Type && x.Level == troops.Level).Attack 
+                                            * 6 * troops.Quantity;      //total damage is calculated based on attack * quantity * 6
+                        }
+                        var defender = DbContext.Kingdoms.Where(x => x.Id == battle.DefenderId).Include(x => x.Troops)
+                                        .ThenInclude(x => x.TroopType).Include(x => x.Resources).FirstOrDefault();
 
-                    totalDefense = defender.Troops.Sum(x => x.TroopType.Defense) + defender.Troops.Sum(x => x.TroopType.Hp);
+                        //total defense is troops HP + defense
+                        totalDefense = defender.Troops.Sum(x => x.TroopType.Defense) + defender.Troops.Sum(x => x.TroopType.Hp);
                     
-                    if (totalDamage >= totalDefense) //if attacker won then true
-                    {
-                        int goldStolen;
-                        int foodStolen;
-                        int impactDamage = (totalDamage - totalDefense) * 10;
+                        if (totalDamage >= totalDefense) //if attacker won then true
+                        {
+                            int goldStolen;
+                            int foodStolen;
+                            int difference = totalDamage - totalDefense;
+                            int impactDamage = (difference) * 10;
                         
-                        //check how many golds and food player have if impact damage is greater than amount then steal all
-                        if (impactDamage >= defender.Resources.FirstOrDefault(x => x.Type == "gold").Amount)    
-                        {
-                            goldStolen = defender.Resources.FirstOrDefault(x => x.Type == "gold").Amount;     
-                        }
-                        else
-                        {
-                            goldStolen = impactDamage;
-                        }
-                        if (impactDamage >= defender.Resources.FirstOrDefault(x => x.Type == "food").Amount)
-                        {
-                            foodStolen = defender.Resources.FirstOrDefault(x => x.Type == "food").Amount;
-                        }
-                        else
-                        {
-                            foodStolen = impactDamage;
-                        }
+                            //check how many golds and food player have if impact damage is greater than amount then steal all
+                            if (impactDamage >= defender.Resources.FirstOrDefault(x => x.Type == "gold").Amount)    
+                            {
+                                goldStolen = defender.Resources.FirstOrDefault(x => x.Type == "gold").Amount;     
+                            }
+                            else
+                            {
+                                goldStolen = impactDamage;
+                            }
+                            if (impactDamage >= defender.Resources.FirstOrDefault(x => x.Type == "food").Amount)
+                            {
+                                foodStolen = defender.Resources.FirstOrDefault(x => x.Type == "food").Amount;
+                            }
+                            else
+                            {
+                                foodStolen = impactDamage;
+                            }
 
-                        //update battle and time when troops get back to town
-                        battle.GoldStolen = goldStolen;
-                        battle.FoodStolen = foodStolen;
-                        battle.Result = "attacker won";
-                        long travelTime = battle.FinishedAt - battle.StartedAt - (GetUnixTimeNow() - battle.FinishedAt);
-                        battle.FinishedAt += travelTime;
+                            //update battle and time when troops get back to town
+                            battle.GoldStolen = goldStolen;
+                            battle.FoodStolen = foodStolen;
+                            battle.Result = "attacker won";
+                            long travelTime = battle.FinishedAt - battle.StartedAt - (GetUnixTimeNow() - battle.FinishedAt);
+                            battle.FinishedAt += travelTime;
                         
-                        DbContext.Battles.Update(battle);
-                        DbContext.SaveChanges();
+                            DbContext.Battles.Update(battle);
+                            DbContext.SaveChanges();
 
-                        //TODO dead troops list and decrease number of resources for defender
+                            //update resources for defender
+                            defender.Resources.FirstOrDefault(x => x.Type == "gold").Amount -= goldStolen;
+                            defender.Resources.FirstOrDefault(x => x.Type == "food").Amount -= foodStolen;
+                        
+                            DbContext.Update(defender);
+                            DbContext.SaveChanges();
+
+                            //saving dead troops list of DEFENDER and remove from kingdom
+                            SaveAndRemoveTroopsLost(defender.Troops);
+
+                            //saving dead troops list of ATTACKER and remove from kingdom
+                            double points = difference;
+                            double quantity;
+                            int attResult;
+                            if (points < 1000)
+                            {
+                                var attackerTroops = battle.AttackingTroops;
+                                foreach (var troop in attackerTroops)
+                                {
+                                    quantity = troop.Quantity;
+                                    attResult = Convert.ToInt32(Math.Round(quantity * (difference / 100)));
+                                    if (attResult < troop.Quantity) //if attResult is higher then all troops survived
+                                    {
+
+                                    }
+                                }
+                            }
+                        }
+                        else    //defender won
+                        {
+                            //TODO dead troops list, resources stolen stays on 0
+                        }
                     }
-                    else    //defender won
+                    if (battle.Result is not null && battle.FinishedAt <= GetUnixTimeNow())
                     {
-                        //TODO dead troops list, resources stolen stays on 0
+                        //TODO add stolen resources, delete troops based on troopsLost, battle status (on way attack, on way home, done)
                     }
+
                 }
-                if (battle.Result is not null && battle.FinishedAt <= GetUnixTimeNow())
+            }
+        }
+        public void SaveAndRemoveTroopsLost(List<Troop> input)
+        {
+            var deadTroops = new List<TroopsLost>();
+            foreach (var troop in input)
+            {
+                if (deadTroops.Any(x => x.Type == troop.TroopType.Type))
                 {
-                    //TODO add stolen resources, delete troops based on troopsLost, battle status (on way attack, on way home, done)
+                    deadTroops.FirstOrDefault(x => x.Type == troop.TroopType.Type).Quantity++;
                 }
+                else
+                {
+                    deadTroops.Add(new TroopsLost()
+                    {
+                        Type = troop.TroopType.Type,
+                        Quantity = 1
+                    });
+                }
+            }
+
+            foreach (var troops in deadTroops)
+            {
+                DbContext.TroopsLost.Add(troops);
+                DbContext.SaveChanges();
+            }
+
+            foreach (var troop in input)
+            {
+                DbContext.Remove(troop);
+                DbContext.SaveChanges();
             }
         }
     }
